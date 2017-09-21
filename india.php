c543655ffe4a74f96d6b633621826cabcc2e78a0
Antony Kodinye <akodinye@gmail.com>
2017-09-21 09:44:53 +0200

    DB Connection Request

---
+++ india.php
@@ -0,0 +1,13 @@
+<?php
+// Create connection
+$conn = mysqli_connect("localhost", "id2982644_aokod1", "Monz36lo!","id2982644_filled");
+
+// Check connection
+if ($conn)
+{
+    echo "Connected successfully";
+} else
+{
+echo "No hoe";
+}
+?>
